6df91da387377947081188a7a400ceaac411d3a2
HYDROK-MK4\hydrok_MK4 <dave.hardie@gmail.com>
2019-02-04 17:04:10 -0500

    switching ChooseNextWayPoint to C++

---
+++ Source/S05_TestingGrounds/Private/ChooseNextWayPoint.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWayPoint.h"
+
+EBTNodeResult::Type UChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return EBTNodeResult::Succeeded;
+}
